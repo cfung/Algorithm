45b97496095c2250554eed3c5d3bc0a11db02202
cfung <chesterfung@gmail.com>
2017-02-15 14:04:38 -0800

    init attempt

---
+++ practice_reverse_string.py
@@ -0,0 +1,21 @@
+my_string = "This is such a cutie cat."
+
+result = ""
+
+len_mystring = len(my_string.split(' '))
+
+for idx, word in enumerate(my_string.split(' ')):
+
+	if idx == len_mystring -1:
+
+		result += word[len(word)-2::-1]
+		result += "."
+		
+	else:
+		#result.append(word[::-1])
+		#result.append(' ')
+		result += word[::-1]
+		result += " "
+		
+
+print str(result)
