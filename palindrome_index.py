fbdb71152281fd5eb0423314032782291894c436
cfung <chesterfung@gmail.com>
2017-02-28 16:24:18 -0800

    checkin working solution

--- palindrome_index.py
+++ palindrome_index.py
@@ -1,32 +1,41 @@
 # mistake 1: (method 2) end of loop should -1
 # mistake 2: (method 2) end of loop forgot idx
+# mistake 3:  cannot enumerate len(input_str)/2, use range
+# mistake 4:  if (s[:idx]+s[idx+1:] == (s[:idx]+s[idx+1:])[::-1]):
+#             figure out how to remove the idx in substring
 
 n = int(raw_input().strip())
 
-def palindrome(input_str):
+def palindrome(s):
     
-    if input_str == input_str[::-1]:
+    if s == s[::-1]:
         return -1
+    else:
+        # save time by only going thru 1/2 of the len (only if compare first and last char)
+        for idx in range(int(len(s)/2)):
+            
+            # method 1: compare entire strings (this did not pass last tc due to runtime error)
+            #newstr = input_str[:idx] + input_str[idx+1:]
 
-    for idx, char in enumerate(len(input_str)/2:
+            #if newstr == newstr[::-1]:
+            #    return idx
 
-        # method 1: compare entire strings
-        #newstr = input_str[:idx] + input_str[idx+1:]
+            # method 2: compare char only, start and end char
+            if s[idx] != s[len(s)-1-idx]:
+                # remove that char, then compare the strings
+ 
+                if (s[:idx]+s[idx+1:] == (s[:idx]+s[idx+1:])[::-1]):
+                #if input_str[idx+1:] == input_str[idx+1:][::-1]:#input_str[len(input_str)-1-idx][::-1]:
+                    return idx
+                else:
+                    return len(s)-1-idx
+     
+                #return (idx if ((s[:idx]+s[idx+1:]) == (s[:idx]+s[idx+1:])[::-1]) else len(s)-1-idx)
+                
 
-        #if newstr == newstr[::-1]:
-        #    return idx
-
-        # method 2: compare start and end char
-        if input_str[idx] != input_str[len(input_str)-1]:
-            if input_str[idx+1] == input_str[len(input_str)-1-idx]:
-                return idx
-            else:
-                return len(input_str)-1-idx
-      
 for i in range(0, n):
     line = raw_input().strip()
     print palindrome(line)
-
 
 # sample input: 'aaab'
 # sample output: 3
@@ -37,24 +46,7 @@ for i in range(0, n):
 # sample input: 'aaa'
 # sample output: -1
 
-'''
-n=int(raw_input())
-for n0 in range(n):
-    s=list(raw_input())
-    if list(reversed(s))==s:
-        print -1
-    else:
-        for _ in range(1,(len(s)/2)+1):
-            if s[_-1]!= s[-_]:
-                break
-        s1=s[:]
-        del s[_-1]
-        del s1[-_]
-        if list(reversed(s))==s:
-            print _-1
-        else:
-            print len(s)+1-_
-'''
+
 
 '''  test case #2
 10
