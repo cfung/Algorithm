da136b4930273bcc398e2689fae801b7ed9c4f2c
cfung <chesterfung@gmail.com>
2017-02-28 18:32:10 -0800

    reverse string without using reverse or [::-1]

---
+++ practice_reverse_string3.py
@@ -0,0 +1,42 @@
+# reverse a string without using any builtin method
+
+# mistake 1: how to start range() and go reverse?
+# mistake 2: forgot to add return
+
+'''
+def practice_reverse_string3(str):
+	
+	result = []
+
+	for i, val in enumerate(str[::-1]):
+		result.append(val)
+
+	return "".join(result)
+
+print practice_reverse_string3("cat")
+print practice_reverse_string3("iamaverycutecat")
+print practice_reverse_string3("I know how it feels.")
+'''
+
+'''
+Cannot use [::-1] or reverse
+
+http://stackoverflow.com/questions/18686860/reverse-a-string-in-python-without-using-reversed-or-1
+
+'''
+# mistake 1: start and end index
+# mistake 2: range returns int rather than value (char), use str[i]
+
+def practice_reverse_string3(str):
+
+	result = []
+
+	for i in range(len(str)-1, -1, -1):
+		result.append(str[i])
+
+	#print "result..", result
+	return "".join(result)
+
+print practice_reverse_string3("cat")
+print practice_reverse_string3("iamaverycutecat")
+print practice_reverse_string3("I know how it feels.")
