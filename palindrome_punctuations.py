0ddb970f899871ef267219c9af4801325d219f84
Chester Fung <cfung@netflix.com>
2018-10-15 17:46:51 -0700

    add test cases

--- palindrome_punctuations.py
+++ palindrome_punctuations.py
@@ -1,7 +1,10 @@
+# mistake 1: use .join() to remove whitespace
+# mistake 2: only used .strip(), did not consider whitespace in middle
+# mistake 3: did not know how to use maketrans() and translate()
 
+import string
 
-
-def isPalindrome(self, s):
+def isPalindrome(s):
     """
     :type s: str
     :rtype: bool
@@ -13,3 +16,7 @@ def isPalindrome(self, s):
     #print("1", s.replace(" ", "").maketrans("","", string.punctuation).lower())
     #print("2", s.replace(" ", "")[::-1].maketrans("", "", string.punctuation).lower())
     return s.replace(" ", "").translate(translation).lower() == s.replace(" ", "").translate(translation)[::-1].lower()
+
+
+assert isPalindrome("A man, a plan, a canal: Panama") == True    
+assert isPalindrome("race a car") == False    
